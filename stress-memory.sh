41e204f309bdf8f59a8b8c2f30cb639d5dc69e65
seriatin <seriatin@hanmail.net>
2019-06-28 15:05:35 +0900

    스트레스테스트-메모리

---
+++ stress-memory.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+banner() {
+    echo "+------------------------------------------------------------------------+"
+    printf "| %-70s |\n" "`date`"
+    echo "|                                                                        |"
+    printf "|`tput bold` %-70s `tput sgr0`|\n" "$@"
+    echo "+------------------------------------------------------------------------+"
+}
+
+one_banner() {
+    printf "`tput bold` %-70s `tput sgr0`\n" "`date` $@"
+}
+
+VM_WORKER=$1
+VM_BYTES=$2
+BACKOFF=${3:-10000}
+TIMEOUT=${4:-600}
+
+banner "Start StressTest ... [MEMORY]"
+
+stress-ng --vm $VM_WORKER --vm-bytes $VM_BYTES --backoff $BACKOFF --timeout $TIMEOUT --metrics -v &
+
+JOBID=$!
+
+while kill -0 "$JOBID"; do
+    one_banner "Running StressTest ... [MEMORY Usage - $(awk '/MemTotal/{t=$2}/MemAvailable/{a=$2}END{print 100-100*a/t"%"}' /proc/meminfo)]"
+    sleep 1
+
+done
+
+banner "Stop StressTest ... [MEMORY]"
+
+
+while true
+do
+    one_banner "Wait Scale-in ..."
+    sleep 10
+done
